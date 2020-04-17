f8521258b70ba305f4dae007a1bbdf2450be8c40
Qaiser Shabbir <qaiser.shabbir55@gmail.com>
2020-04-16 19:00:19 -0500

    Commit 2 Remaining Tasks: 1. Input checks need to be verified 2. Input statements need to be corrected 3. Edge cases needs to be tested 4. Number of maximum steps needs to be fixed

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 import random
 import sys
 
+
 class Board:
     def __init__(self, size=4):
         self.size = size
@@ -19,7 +20,7 @@ class Board:
             for j in range(self.size):
                 if self.config[i][j] == 'x':
                     if self.conflicts[i][j] > 0:
-                        index.append((i,j))
+                        index.append((i, j))
         return index
 
     def goal_test(self):
@@ -29,8 +30,45 @@ class Board:
                 if self.config[i][j] == 'x':
                     score += self.conflicts[i][j]
 
-        if score: return False
+        if score:
+            return False
         return True
+
+    def print_config(self, flag):
+        if flag:
+            print()
+            print("---------------------------------------------------------------------------------------------------")
+            print("Final Config of the board:")
+            print()
+
+            for i in range(self.size):
+                print("| ", end="")
+
+                for j in range(self.size):
+                    print(self.config[i][j], end=" | ")
+                print()
+
+                for k in range(self.size * 4):
+                    print("-", end="")
+                print()
+
+        else:
+            print()
+            print("---------------------------------------------------------------------------------------------------")
+            print("Solution could not be found in defined limit. Max steps used = 1000")
+            print("---------------------------------------------------------------------------------------------------")
+
+    def find_min_column(self, row, col):
+        min_val = sys.maxsize
+        min_index = row
+        for i in range(self.size):
+            if i == row:
+                continue
+            else:
+                if self.conflicts[i][col] < min_val:
+                    min_val = self.conflicts[i][col]
+                    min_index = i
+        return min_index
 
     def calc_attack(self, i, j):
         attacks = 0
@@ -44,7 +82,7 @@ class Board:
 
         # Horizontal attack from right
         cols = j
-        while cols < self.size-1:
+        while cols < self.size - 1:
             cols += 1
             if self.config[i][cols] == 'x':
                 attacks += 1
@@ -61,7 +99,7 @@ class Board:
         # Diagonal downward right
         rows = i
         cols = j
-        while rows < self.size-1 and cols < self.size-1:
+        while rows < self.size - 1 and cols < self.size - 1:
             rows += 1
             cols += 1
             if self.config[rows][cols] == 'x':
@@ -79,71 +117,38 @@ class Board:
         # Diagonal upward right
         rows = i
         cols = j
-        while rows > 0 and cols < self.size-1:
+        while rows > 0 and cols < self.size - 1:
             rows -= 1
             cols += 1
             if self.config[rows][cols] == 'x':
                 attacks += 1
 
-        # print(f"Attack on {i,j} = {attacks}")
         return attacks
-
-    def print_config(self, flag):
-        if flag:
-            print("Final Config of the board:")
-            print()
-            for i in range(self.size):
-                print("| ", end="")
-                for j in range(self.size):
-                    print(self.config[i][j], end=" | ")
-                print()
-        else:
-            print("Solution could not be found in defined Maximum number of steps")
-
-    def find_min_column(self, row, col):
-        min = sys.maxsize
-        min_index = row
-        for i in range(self.size):
-            if i == row:
-                continue
-            else:
-                if self.conflicts[i][col] < min:
-                    min = self.conflicts[i][col]
-                    min_index = i
-        return min_index
-
-
-
-
 
 
 class SearchSolution:
     def __init__(self, max_steps):
         self.board = SearchSolution.take_input_config()
-        # self.board.print_config(True)
         self.max_steps = max_steps
         self.board.find_conflicts()
 
     def initialize_game(self):
         step = 0
+
         while step < self.max_steps:
             if self.board.goal_test():
-                # print("test success")
                 self.board.print_config(True)
                 return
+
             else:
                 conflicted = self.board.conflicted_index()
-                row,col  = random.choice(conflicted)
+                row, col = random.choice(conflicted)
                 new_row = self.board.find_min_column(row, col)
                 self.board.config[row][col] = 'o'
                 self.board.config[new_row][col] = 'x'
-
                 self.board.find_conflicts()
-                #pick a random queen and move it to min conflict place and recalculate
-                # Repeat until max steps or goal true, whichever comes first
-                #print solution
-                pass
             step += 1
+
         else:
             self.board.print_config(False)
 
@@ -155,10 +160,10 @@ class SearchSolution:
                 if size > 0:
                     break
                 else:
-                    print("Entered invalid size value")
+                    print("Size is not valid. Please enter again.")
                     continue
             except ValueError:
-                print("Entered value is not a valid integer. Please enter again. ")
+                print("Size is not valid. Please enter again. ")
                 continue
 
         board = Board(size)
@@ -166,19 +171,20 @@ class SearchSolution:
         for j in range(size):
             while True:
                 try:
-                    i = int(input(f"Please enter the row number for queen in column {j} : "))
-                    if i < size:
-                        board.config[i][j] = 'x'
+                    i = int(input(f"Enter Location of Queen in Column {j + 1}:({1} - {size}) : "))
+                    if 0 <= i - 1 < size:
+                        board.config[i - 1][j] = 'x'
                         break
                     else:
-                        print("Entered Index value is incorrect")
+                        print("Invalid index entered. Please enter again.")
                         continue
                 except ValueError:
-                    print(f"Entered value is not a valid index, row number ranges from 0 to {size-1} : ")
+                    print(f"Invalid index entered. Please enter again. ")
                     continue
 
         return board
 
+
 class Main:
-    ss = SearchSolution(500)
+    ss = SearchSolution(1000)
     ss.initialize_game()
